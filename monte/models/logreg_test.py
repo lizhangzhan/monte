2887efe6ca0d59cc4e755939f0f322a2649ebad1
zhanlizhang <zhanlizhang@bytedance.com>
2014-05-19 21:13:18 +0800

    commit monte machine learning library

---
+++ monte/models/logreg_test.py
@@ -0,0 +1,9 @@
+from logreg import Logreg
+from numpy.random import randn
+import numpy as np
+
+X = randn(5, 10)
+Y = np.array([[1,0],[1,0], [1,0],[1,0],[1,0],\
+        [0,1],[0,1],[0,1],[0,1],[0,1]])
+
+
